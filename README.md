129c015f40fd6468bba6ed92a5ecf360fc8ab8f5
Donovan Gervais <applemac@Donovans-MacBook-Pro-3.local>
2020-11-02 15:08:44 +0000

    python varibles with int and type()

---
+++ README.md
@@ -0,0 +1,16 @@
+# Why Python
+## pycharm setup
+### python 3.9
+### Python Variable
+
+- What is a variable
+- Dynamically typed languages
+- Overwriting variables
+- Getting user input from the command line
+
+# Variable:
+- is a place holder to store data
+
+# Sudo Coding
+- It is a translation of your code
+- This is very handy due to the fact that business and product owners don't always know the technical terms for code.
--- python_variables.py
+++ python_variables.py
@@ -1,1 +1,28 @@
-print('hello world!!!!!')
+# How can we create a variable
+#
+
+# name = "Donovan"
+# Creatinf a variable called name to store user name
+
+age = 18  # Integer
+hourly_wage = 5  # Integer
+
+travel_allowance = 2.4  #Float
+
+# Types of variables in python
+# Strings, Integers, Floats, Boolean
+
+print(travel_allowance)
+print(age)
+print(hourly_wage)
+
+# print(name)
+
+# How can we find out the type of variable
+# We can use the type() method
+
+# How can we take user data
+# We use the method called input() to get data from user
+
+# name = input("Please Enter Your Name")
+
