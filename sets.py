88e40d1816ac3fcc6d646b3902e20207706504cc
Donovan Gervais <applemac@Donovans-MacBook-Pro-6.local>
2020-11-03 15:03:16 +0000

    sets add and remove

---
+++ sets.py
@@ -0,0 +1,17 @@
+# set - data collection with sets
+# indexing - THERE IS NO INDEXING IN SETS
+# sets are unordered
+# Syntax: {} brackets for sets
+
+car_parts = {"WHeels", "Doors", "Radio"}
+print(car_parts)
+
+# managing data with sets
+
+# add an item to the set
+car_parrs.add("seat  belt")
+print(car_parts)
+
+# delete and item from the set
+car_parts.remove("Wheels")
+print(car_parts)
